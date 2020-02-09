6f6de3d0bb67f66e9c1db0e0a75af8edc7f54db4
Joshua Grossman <Josh1794@gmail.com>
2020-02-09 13:48:49 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
+
+function tick() {
+  const element = (
+    <div>
+      <h1>It is {new Date().toLocaleTimeString()}.</h1>
+    </div>
+  );
+  ReactDOM.render(element, document.getElementById("root"));
+}
+
+setInterval(tick, 1000);
